4551bbae38872873fac0b27675ee7bac71c87eff
Santiago Mozo <smozo615@gmail.com>
2022-11-11 00:49:19 -0500

    Auth service: Custom Error Handler
      - 'src/errors/':
        * Create abstract class CustomError.
        * Create class RequestValidationError.
    
      - 'src/middlewares/':
        * Add validation: 'instanceof' to error-handler.
        * Add 'RequestValidationError' to express-validator.

--- auth/src/middlewares/express-validator-handler.ts
+++ auth/src/middlewares/express-validator-handler.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 
+import { RequestValidationError } from '../errors/request-validation-error';
+
 export const ExpressValidator = (
   req: Request,
   res: Response,
@@ -8,7 +10,7 @@ export const ExpressValidator = (
 ) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new Error('Invalid Request');
+    throw new RequestValidationError(errors.array());
   }
   next();
 };

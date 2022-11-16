62f08b31d7d4cdd11ff1ba46dd55dd0ddd8f9407
Santiago Mozo <smozo615@gmail.com>
2022-11-15 23:26:40 -0500

    Auth service: user profile
      - "src/middlewares":
        * Create 'current-user' and 'require-auth'.
    
      - "src/errors":
        * Create 'not-authorized-error'.
    
      - "src/routes/profile"
        * Add middleware: 'current-user' to route

--- auth/src/routes/profile.ts
+++ auth/src/routes/profile.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/profile', (req, res) => {
-  res.json({ route: 'profile' });
-});
+router.get(
+  '/api/users/profile',
+  currentUser,
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as profileRouter };

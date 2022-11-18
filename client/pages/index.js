1806d69fa8c2e1bba599b2d0d0d2f37867b24988
Santiago Mozo <smozo615@gmail.com>
2022-11-17 23:39:55 -0500

    Client service: Create request context
      - Create 'api/build-client'
      - Add 'build-client' to 'pages/index'

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,14 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
-}
+import buildClient from '../api/build-client';
+
+const HomePage = ({ currentUser }) => {
+  return <h1>{currentUser.email}</h1>;
+};
+
+HomePage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/profile');
+
+  return data;
+};
 
 export default HomePage;

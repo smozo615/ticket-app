c1856344c9f0b3ab4b3cde9493b32f0531cae4b6
Santiago Mozo <smozo615@gmail.com>
2022-11-20 17:48:56 -0500

    Tickets service: Create controller for route GET /api/tickets

---
+++ tickets/src/routes/tickets.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.get(
+  '/api/tickets',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const tickets = await Ticket.find();
+      res.send(tickets);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+export { router as TicketsRouter };

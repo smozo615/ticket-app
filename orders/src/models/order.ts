dd506a03bc5454b6476ef8b1382af9d6661ee2ee
Santiago Mozo <smozo615@gmail.com>
2022-12-24 22:52:12 -0500

    Order service: Order and Ticket models
      - Create src/models
      - Update @sm-ticket-app/common and
        import the enum to the order model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@sm-ticket-app/common';
+
+import { TicketDoc } from './ticket';
+
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: string;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: string;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const ordersSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ordersSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', ordersSchema);
+
+export { Order };

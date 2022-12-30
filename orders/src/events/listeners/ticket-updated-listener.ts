c9b55031381c164ca0609a6836105a1f892738f4
Santiago Mozo <smozo615@gmail.com>
2022-12-29 19:09:04 -0500

    Order service: ticket updated listener
      - Create class TicketUpdatedListener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,24 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketUpdatedEvent } from '@sm-ticket-app/common';
+
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = await Ticket.findById(id);
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}

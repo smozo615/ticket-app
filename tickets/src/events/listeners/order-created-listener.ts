079da2fc02bb0311776d7e2eb245e6c35a0c80a9
Santiago Mozo <smozo615@gmail.com>
2022-12-30 22:16:22 -0500

    ticket service: order-created-listener
      - Update ticketSchema
      - Update "@sm-ticket-app/common"
      - Create listener for order created event

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,40 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@sm-ticket-app/common';
+import { Message } from 'node-nats-streaming';
+
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Store the ticket
+    await ticket.save();
+
+    // Emit ticket updated event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId,
+    });
+
+    // Ack the message
+    msg.ack();
+  }
+}
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -16,6 +16,7 @@ interface TicketDoc extends mongoose.Document {
   price: number;
   userId: string;
   version: number;
+  orderId?: string;
 }
 
 // An interface that describes the properties
@@ -28,6 +29,7 @@ const ticketSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     userId: { type: String, required: true },
+    orderId: { type: String },
     price: { type: Number, required: true },
   },
   {

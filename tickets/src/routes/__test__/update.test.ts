f0d9366f0021c2619fa9adbd413d61adad5da0b1
Santiago Mozo <smozo615@gmail.com>
2022-12-30 23:13:08 -0500

    Ticket service: test for reject updates if ticket is reserved
      - Add new test for update.ts

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 // To ensure mock invocations (nats events)
 import { natsWrapper } from '../../nats';
@@ -143,4 +144,31 @@ it('publishes a updated ticket event', async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates if the ticket is reserved', async () => {
+  const cookie = await global.cookie();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'title',
+      price: 20,
+    })
+    .expect(201);
+
+  // Reserved ticket
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'title updated',
+      price: 50,
+    })
+    .expect(400);
 });

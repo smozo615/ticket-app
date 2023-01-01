8ccff480d630ecf1526928854e9fac79fb72633b
Santiago Mozo <smozo615@gmail.com>
2023-01-01 17:56:55 -0500

    Payments service: order model

---
+++ payments/src/models/orders.ts
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { OrderStatus } from '@sm-ticket-app/common';
+export { OrderStatus };
+
+interface OrderAttrs {
+  id: string;
+  userId: string;
+  status: OrderStatus;
+  version: number;
+  price: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  price: number;
+  version: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const ordersSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ordersSchema.set('versionKey', 'version');
+ordersSchema.plugin(updateIfCurrentPlugin);
+
+ordersSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', ordersSchema);
+
+export { Order };

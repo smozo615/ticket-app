7e06146c8b6f6868c10e10bdd27d9aeb739e1585
Santiago Mozo <smozo615@gmail.com>
2023-01-02 00:15:07 -0500

    Payments service: route POST '/api/payments'
      - Add handler to new.ts

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,7 +1,14 @@
-import { requireAuth, validateRequest } from '@sm-ticket-app/common';
+import {
+  BadRequestError,
+  ForbiddenError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@sm-ticket-app/common';
 import express, { Request, Response, NextFunction } from 'express';
 import { checkSchema } from 'express-validator';
 
+import { Order, OrderStatus } from '../models/orders';
 import { CreateChargeSchema } from '../utils/validator-schemas/create-order-schema';
 
 const router = express.Router();
@@ -13,6 +20,21 @@ router.post(
   validateRequest,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const { token, orderId } = req.body;
+
+      const order = await Order.findById(orderId);
+      if (!order) {
+        throw new NotFoundError('Order not found');
+      }
+
+      if (order.userId !== req.currentUser!.id) {
+        throw new ForbiddenError('Forbidden resource');
+      }
+
+      if (order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError('Cannot pay for a cancelled order');
+      }
+
       res.send({ success: true });
     } catch (err) {
       next(err);

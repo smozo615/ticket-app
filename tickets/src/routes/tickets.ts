18496cf42ef540c08a2519fd5de64e3cd0d881bf
Santiago Mozo <smozo615@gmail.com>
2023-01-05 17:11:54 -0500

    Tickets service: filter tickets reserved

--- tickets/src/routes/tickets.ts
+++ tickets/src/routes/tickets.ts
@@ -8,7 +8,7 @@ router.get(
   '/api/tickets',
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const tickets = await Ticket.find();
+      const tickets = await Ticket.find({ orderId: undefined });
       res.send(tickets);
     } catch (err) {
       next(err);

379066afcc04eb21cf7dbb19930a5ac6fa5c5c09
Santiago Mozo <smozo615@gmail.com>
2023-01-04 20:49:52 -0500

    Client: ticket show
      - Route: /tickets/:ticketId

---
+++ client/pages/tickets/[ticketId].js
@@ -0,0 +1,16 @@
+const ticketShow = ({ ticket }) => {
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+    </div>
+  );
+};
+
+ticketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+  return { ticket: data };
+};
+
+export default ticketShow;
